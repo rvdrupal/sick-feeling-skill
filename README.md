93a4498585b82bbef9587e14357533cb18ed993d
Rohit Yadav <ds.rohityadav@gmail.com>
2021-05-06 12:05:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/robot.svg" card_color="#22A7F0" width="50" height="50" style="vertical-align:bottom"/> Sick Feeling
+Feeling check
+
+## About
+Bot to check data on customer feelings.
+
+## Examples
+* "I am {f}"
+
+## Credits
+rohit
+
+## Category
+**Daily**
+Configuration
+IoT
+
+## Tags
+#Doctor
+#Ill
+#Sick
+#Health
+
---
+++ __init__.py
@@ -0,0 +1,19 @@
+from mycroft import MycroftSkill, intent_file_handler
+
+
+class SickFeeling(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+
+    @intent_file_handler('feeling.sick.intent')
+    def handle_feeling_sick(self, message):
+        f = message.data.get('f')
+
+        self.speak_dialog('feeling.sick', data={
+            'f': f
+        })
+
+
+def create_skill():
+    return SickFeeling()
+
